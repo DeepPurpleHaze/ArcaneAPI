77a134dcd5863ddd5cd67e5dd16783b1f518fe62
Pavel Plakhotniuk <pavel.plachotnjuk@gmail.com>
2019-08-07 20:15:10 +0300

    ArticleTypesController

---
+++ ArcaneAPI/Controllers/ArticleTypesController.cs
@@ -0,0 +1,36 @@
+﻿using ArcaneAPI.Models.CustomModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace ArcaneAPI.Controllers
+{
+    [RoutePrefix("api/ArticleTypes")]
+    public class ArticleTypesController : ApiController
+    {
+        private ArticleTypeRepository Repository = new ArticleTypeRepository();
+
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("GetAll")]
+        [ResponseType(typeof(IEnumerable<ArticleTypeDTO>))]
+        public IHttpActionResult GetArticlesByType(int articleTypeId)
+        {
+            try
+            {
+                IEnumerable<ArticleTypeDTO> articleTypes = Repository.Get().Select(d => d.DTO);
+                return Ok(articleTypes);
+            }
+            catch (NullReferenceException nre)
+            {
+                return NotFound();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
